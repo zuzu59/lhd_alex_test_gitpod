fea50a50d36dec1a72d6f27a5f24d925150c9fb4
Christian Zufferey <christian@zufferey.com>
2021-11-05 15:24:49 +0000

    first commit

---
+++ start.sh
@@ -0,0 +1,13 @@
+start.sh
+
+#!/bin/bash
+#Petit script pour démarrer tout le binz
+#zf211105.1224
+
+
+sudo apt update ; sudo apt-get install apt-transport-https ca-certificates -y ; sudo update-ca-certificates
+git config --global credential.helper wincred
+
+mkdir alex
+cd alex
+git clone https://gitlab.epfl.ch/javet/lh3.frontend.git
